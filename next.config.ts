b16e9d18d508e053c56cd21ac7608fa87d17ff76
odynamic <ghaniyaputridyah@gmail.com>
2026-06-22 01:21:16 +0700

    fresh install and fix local environment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Matikan semuanya
   eslint: {
+    // Ini akan menonaktifkan ESLint selama proses build di Vercel
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Ini juga akan menonaktifkan pemeriksaan tipe TypeScript yang ketat
     ignoreBuildErrors: true,
   },
 };
+
+export default nextConfig;

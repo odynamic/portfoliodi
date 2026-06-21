e19e404b60a0a522af5ca95c9d7be4c6bef14d85
odynamic <ghaniyaputridyah@gmail.com>
2026-06-22 00:33:58 +0700

    update next config untuk bypass eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Menonaktifkan pengecekan ESLint saat proses build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
